0673a70842004ddd46c05f093075126a899df0b5
9ine4our <148853314+9ine4our@users.noreply.github.com>
2025-01-28 14:29:43 -0800

    Create cont.js

---
+++ website/js/cont.js
@@ -0,0 +1,28 @@
+document.getElementById('content').innerHTML = `
+<header>
+    <h1>Halo content!</h1>
+</header>
+<main>
+    <p>Ini adalah halaman HTML dasar. Anda dapat mengeditnya sesuai kebutuhan.</p>
+    <p>Kunjungi <a href="https://example.com" target="_blank">Example</a> untuk melihat contoh lainnya.</p>
+</main>
+<footer>
+    <p>&copy; 2025, Semua Hak Dilindungi.</p>
+</footer>
+
+<div id="dt_contenedor">
+    <header id="header" class="main">
+      <div class="hbox">
+        <div class="logo">
+          <a href="javascript:void(0);"><img alt="Movie &amp; TV Series Stream" src="https://static-web.prod.vidiocdn.com/_next/static/logo/vidio.png"></a>
+        </div>
+      </div>
+    </header>
+    <div class="fixheadresp">
+      <header class="responsive">
+        <div class="logo">
+          <a href="javascript:void(0);"><img alt="Movie &amp; TV Series Stream" src="https://static-web.prod.vidiocdn.com/_next/static/logo/vidio.png"></a>
+        </div>
+      </header>
+    </div>
+`;
