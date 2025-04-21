8505d9528ea8282a90cef54dd6b202bcae9e13e4
9ine4our <148853314+9ine4our@users.noreply.github.com>
2025-04-21 04:32:35 -0700

    Create array.js

---
+++ test/array.js
@@ -0,0 +1,8 @@
+    let videoDataArray = [];
+
+    if (videoDataArray.length === 0) {
+      videoDataArray = [
+        { title: "Episode 1", img: "https://via.placeholder.com/150", url: "https://example.com/video1" },
+        { title: "Episode 2", img: "https://via.placeholder.com/150", url: "https://example.com/video2" },
+      ];
+    }
