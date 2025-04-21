1336d19a61a999bf75be7c375caa5a25d3e27e37
9ine4our <148853314+9ine4our@users.noreply.github.com>
2025-04-21 03:33:14 -0700

    Update json.js

--- test/json.js
+++ test/json.js
@@ -1,16 +1,16 @@
     const data = [
       {
-        name: "Kung Fu Hustle(2004)",
+        name: "Kung Fu Hustle (2004)",
         img: "https://image.tmdb.org/t/p/w500/exbyTbrvRUDKN2mcNEuVor4VFQW.jpg",
         url: "https://woneart-bot.github.io/movies/media-player.html?url=https%3A%2F%2Fshort.icu%2FkkQl7XHwf"
       },
       {
-        name: "Cast Away(2000)",
+        name: "Cast Away (2000)",
         img: "https://image.tmdb.org/t/p/w500/7lLJgKnAicAcR5UEuo8xhSMj18w.jpg",
         url: "https://woneart-bot.github.io/movies/media-player.html?url=https%3A%2F%2Fshort.icu%2F8XM2eWSrn"
       },
       {
-        name: "Ratatouille(2007)",
+        name: "Ratatouille (2007)",
         img: "https://image.tmdb.org/t/p/w500/t3vaWRPSf6WjDSamIkKDs1iQWna.jpg",
         url: "https://woneart-bot.github.io/movies/media-player.html?url=https%3A%2F%2Fshort.icu%2FJXL_63CwR"
       }
