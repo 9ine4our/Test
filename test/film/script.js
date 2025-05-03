44fe1bd3704f283987fbff9ca3057100603a700c
9ine4our <148853314+9ine4our@users.noreply.github.com>
2025-05-03 06:24:59 -0700

    Update script.js

--- test/film/script.js
+++ test/film/script.js
@@ -1,4 +1,7 @@
 // fetch('data/films2.json')
+// fetch(
+  "https://rawcdn.githack.com/9ine4our/Test/refs/heads/main/test/film/data/film.json"
+)
 fetch(
   "https://raw.githubusercontent.com/9ine4our/Test/refs/heads/main/test/film.json"
 )
